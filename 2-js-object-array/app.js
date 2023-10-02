1e02690fdceded18cce7c6083e529fc65f03a629
oguzhanuyanik-sr <oguzhanuyanik.sr@gmail.com>
2023-10-02 21:10:11 +0300

    js task done

---
+++ 2-js-object-array/app.js
@@ -0,0 +1,19 @@
+import { products } from './constants.js';
+
+const priceRange30To100 = products.filter(({ unitPrice }) => unitPrice > 30 && unitPrice < 100).length;
+const startsWithA = products.filter((product) => (product.name.startsWith('a')));
+const endsWithA = products.filter((product) => (product.name.slice(-1) === ('a')));
+const id10 = products.filter((product) => product.id.toString().trim() === '10');
+const noDiscontinuedCount = products.filter((product) => !product.discontinued).length;
+const expensiveProduct = products.reduce((max, product) => (max.unitPrice > product.unitPrice) ? max : product);
+const cheapestProduct = products.reduce((min, product) => (min.unitPrice < product.unitPrice) ? min : product);
+
+console.log(`
+# Fiyatı 30-100 arası olan ürün sayısı:\n${priceRange30To100}\n
+# a harfi ile başlayan ürünler:\n${startsWithA.map(item => item.name).join('\n')}\n
+# a harfi ile biten ürünler:\n${endsWithA.map(item => item.name).join('\n')}\n
+# ID değeri 10 olan ürünün adı:\n${id10.map(item => item.name).join('\n')}\n
+# Discontinued false olan ürün sayısı:\n${noDiscontinuedCount}\n
+# En pahalı ürün:\n${expensiveProduct.name} - ${expensiveProduct.unitPrice}\n
+# En ucuz ürün:\n${cheapestProduct.name} - ${cheapestProduct.unitPrice}\n
+`);
